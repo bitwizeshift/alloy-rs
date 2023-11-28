4e6e491bc825fc7898ca4534f810dbd7072b0619
Matthew Rodusek <7519129+bitwizeshift@users.noreply.github.com>
2023-11-27 22:57:22 -0500

    ✨ Add crate for "Alloy"
    This adds the base crate for the actual game engine itself. This will
    ultimately be where the bulk, if not all, of the code will eventually
    live. For now, this just contains the module documentation, and some
    baseline setup for the optional features and dependencies.

---
+++ alloy/src/lib.rs
@@ -0,0 +1,15 @@
+//! The Alloy crate provides the base Alloy Game Engine.
+//!
+//! Alloy is a flexible game-engine and development kit under active
+//! development written in Rust. It is the successor to the C++ project of the
+//! [same name](https://github.com/bitwizeshift/alloy), and exists largely as
+//! an experiment to gain more deep knowledge of working with Rust and rendering
+//! libraries.
+//!
+//! It is characterized by upholding a strict set of design-goals which center
+//! around maintainability and readability through proper software engineering
+//! practices. The overall goal for this project is to be a well-rounded and
+//! easy-to-use, efficient game-engine for 2.5D graphics projects.
+#![deny(missing_docs)]
+#![deny(unused_results)]
+#![deny(rust_2018_idioms)]
