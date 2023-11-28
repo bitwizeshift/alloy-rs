637d974a2d589bcd08a92c3b05506278fa76f91d
Matthew Rodusek <7519129+bitwizeshift@users.noreply.github.com>
2023-11-27 21:08:47 -0500

    ✨ Add vulkan framework infrastructure
    There are various Vulkan crates out there, but all of them are pre-1.0,
    which makes it hard to trust the quality, stability, or maintenance of
    the projects. Thus the decision was made to simply wrap the C API, which
    is rather straight-forward to do already.
    
    This adds a new crate for `vulkan` to handle exactly this.

---
+++ frameworks/vulkan/build.rs
@@ -0,0 +1,45 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+  link_vulkan();
+  generate_vulkan_bindings();
+}
+
+fn link_vulkan() {
+  build::rustc_link_lib!("vulkan");
+  if cfg!(any(target_os = "macos", target_os = "ios")) {
+    build::rustc_link_lib!("MoltenVK");
+  }
+}
+
+fn generate_vulkan_bindings() {
+  build::rerun_if_changed!("../../3rd-party/vulkan-headers/include/vulkan/vulkan.h");
+
+  let builder =
+    bindgen::Builder::default().header("../../3rd-party/vulkan-headers/include/vulkan/vulkan.h");
+
+  let bindings = target_bindgen_args(builder)
+    .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+    .clang_args(&["-I", "../../3rd-party/vulkan-headers/include"])
+    .generate()
+    .expect("Unable to generate bindings");
+
+  // Write the bindings to the $OUT_DIR/bindings.rs file.
+  let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+  bindings
+    .write_to_file(out_path.join("bindings.rs"))
+    .expect("Couldn't write bindings!");
+}
+
+#[cfg(any(target_os = "macos", target_os = "ios"))]
+fn target_bindgen_args(builder: bindgen::Builder) -> bindgen::Builder {
+  let sysroot = build::apple::target_xcode_sysroot();
+
+  builder.clang_args(&["-isysroot", &sysroot])
+}
+
+#[cfg(not(any(target_os = "macos", target_os = "ios")))]
+fn target_bindgen_args(builder: bindgen::Builder) -> bindgen::Builder {
+  builder
+}
---
+++ frameworks/vulkan/src/lib.rs
@@ -0,0 +1,35 @@
+//! This crate provides a wrapper around the underlying Vulkan implementation.
+//!
+//! The raw generated bindings can be found in the [`c`] module, or a more
+//! hierarchical binding may be leveraged from the rest of this crate.
+#![deny(missing_docs)]
+// #![deny(unused_results)]
+#![deny(rust_2018_idioms)]
+
+/// This module provides all that raw definitions of the C Vulkan library.
+pub mod c {
+  #![allow(non_upper_case_globals)]
+  #![allow(non_camel_case_types)]
+  #![allow(non_snake_case)]
+  #![allow(unused)]
+  #![allow(missing_docs)]
+  #![allow(unused_results)]
+  #![allow(rust_2018_idioms)]
+  include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+}
+
+/// Counts the number of extension properties available.
+pub fn count_extension_properties() -> usize {
+  let mut count: u32 = 0;
+
+  // SAFETY: This is only unsafe since it's a C call
+  let r = unsafe {
+    c::vkEnumerateInstanceExtensionProperties(
+      std::ptr::null(),
+      &mut count as *mut u32,
+      std::ptr::null_mut(),
+    );
+  };
+  let _ = r;
+  count as usize
+}
