f0bd4687b6f906c1a924fd5009aef1b20431abc2
Matthew Rodusek <7519129+bitwizeshift@users.noreply.github.com>
2023-11-27 23:02:15 -0500

    ✨ Add very basic `example` project
    This "example" project will be the baseline to build-out the
    functionality of this library. In the mean time, this will be a basic
    application that can be executed and will be checked-in with new
    features. However, in the future, this will be migrated to make use of
    Cargo's real "example" feature.

---
+++ example/src/main.rs
@@ -0,0 +1,39 @@
+fn main() {
+  let context = glfw::Context::new().expect("GLFW should be initialized");
+
+  let raw_window = unsafe {
+    glfw::c::glfwCreateWindow(
+      640,
+      480,
+      "My first window".as_bytes().as_ptr() as *const i8,
+      std::ptr::null_mut(),
+      std::ptr::null_mut(),
+    )
+  };
+  let window = glfw::Window::from_c(raw_window);
+  window.show();
+
+  if context.vulkan_supported() {
+    println!("Vulkan is supported!")
+  } else {
+    println!("Vulkan is not supported!")
+  }
+
+  let count = vulkan::count_extension_properties();
+  println!("Vulkan extensions: {}", count);
+
+  use core::ffi::CStr;
+  let _device = openal::Device::open_default();
+  if openal::has_extension(unsafe {
+    CStr::from_ptr("ALC_ENUMERATION_EXT\0".as_bytes().as_ptr() as *mut i8)
+  }) {
+    println!("OpenAL has enumeration extension!")
+  } else {
+    println!("OpenAL does NOT have enumeration extension!")
+  }
+
+  while !window.should_close() {
+    context.poll_events();
+    window.swap_buffers();
+  }
+}
