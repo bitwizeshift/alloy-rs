dc93a7dc2be888404612df8147c3e814aac25f23
Matthew Rodusek <7519129+bitwizeshift@users.noreply.github.com>
2023-12-17 22:50:37 -0500

    🎯 Add some tests for `Bool32`
    This crate currently doesn't have any real tests; but there are some
    easily testable logic that doesn't have full integrations, such as
    the `Bool32` type. This adds some basic tests for the functionality,
    which also helped uncover some unexpected issues -- such as missing
    functionality, and unstable internal values.

--- frameworks/vulkan/src/primitives/bool32.rs
+++ frameworks/vulkan/src/primitives/bool32.rs
@@ -1,3 +1,5 @@
+use std::str::ParseBoolError;
+
 use crate::c;
 
 /// A 0-cost abstraction wrapper around the underlying [`VkBool32`].
@@ -10,6 +12,12 @@ use crate::c;
 pub struct Bool32(c::VkBool32);
 
 impl Bool32 {
+  /// An easy constant for representing a true [`Bool32`] instance.
+  pub const TRUE: Self = Self::new(true);
+
+  /// An easy constant for representing a false [`Bool32`] instance.
+  pub const FALSE: Self = Self::new(false);
+
   /// Constructs this [`Bool32`] with a boolean value.
   #[inline(always)]
   pub const fn new(b: bool) -> Self {
@@ -27,7 +35,7 @@ impl From<bool> for Bool32 {
 impl From<c::VkBool32> for Bool32 {
   #[inline(always)]
   fn from(value: c::VkBool32) -> Self {
-    Self(value)
+    Self((value != 0) as c::VkBool32)
   }
 }
 
@@ -70,6 +78,20 @@ impl PartialEq<Bool32> for bool {
   }
 }
 
+impl PartialEq<c::VkBool32> for Bool32 {
+  #[inline]
+  fn eq(&self, other: &c::VkBool32) -> bool {
+    self.0 == Self::from(*other)
+  }
+}
+
+impl PartialEq<Bool32> for c::VkBool32 {
+  #[inline]
+  fn eq(&self, other: &Bool32) -> bool {
+    &Bool32::from(*self) == other
+  }
+}
+
 impl std::fmt::Display for Bool32 {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
     if self.0 == 0 { "false" } else { "true" }.fmt(f)
@@ -79,5 +101,57 @@ impl std::fmt::Display for Bool32 {
 impl std::fmt::Debug for Bool32 {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
     write!(f, "Bool32{{{}}}", self)
+  }
+}
+
+impl std::str::FromStr for Bool32 {
+  type Err = ParseBoolError;
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    bool::from_str(s).map(Bool32::new)
+  }
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+  use vulkan_sys as c;
+
+  #[test]
+  fn test_eq() {
+    assert_eq!(Bool32::TRUE, true);
+    assert_eq!(Bool32::FALSE, false);
+    assert_eq!(Bool32::TRUE, 4 as c::VkBool32);
+    assert_eq!(Bool32::FALSE, 0 as c::VkBool32);
+    assert_eq!(true, Bool32::TRUE);
+    assert_eq!(false, Bool32::FALSE);
+    assert_eq!(4 as c::VkBool32, Bool32::TRUE);
+    assert_eq!(0 as c::VkBool32, Bool32::FALSE);
+  }
+
+  #[test]
+  fn test_ne() {
+    assert_ne!(Bool32::TRUE, false);
+    assert_ne!(Bool32::FALSE, true);
+    assert_ne!(Bool32::FALSE, 4 as c::VkBool32);
+    assert_ne!(Bool32::TRUE, 0 as c::VkBool32);
+    assert_ne!(false, Bool32::TRUE);
+    assert_ne!(true, Bool32::FALSE);
+    assert_ne!(4 as c::VkBool32, Bool32::FALSE);
+    assert_ne!(0 as c::VkBool32, Bool32::TRUE);
+  }
+
+  #[test]
+  fn test_display() {
+    assert_eq!(format!("{}", Bool32::from(true)), "true");
+    assert_eq!(format!("{}", Bool32::from(false)), "false");
+    assert_eq!(format!("{}", Bool32::from(42)), "true");
+  }
+
+  #[test]
+  fn test_from_str() {
+    use std::str::FromStr;
+    assert_eq!(Bool32::from_str("true"), Ok(Bool32::TRUE));
+    assert_eq!(Bool32::from_str("false"), Ok(Bool32::FALSE));
+    assert!(Bool32::from_str("no").is_err());
   }
 }
