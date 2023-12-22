882e6b8fa3d319adc565e3e3b3cff770e109fcd4
Matthew Rodusek <7519129+bitwizeshift@users.noreply.github.com>
2023-12-21 20:46:01 -0500

    🩹 Copy `libvulkan` binary to output dir
    Nightly is failing due to the same issue that has been plaguing this
    project for a while, where the `vulkan-sys` project is failing to link
    during tests. This finally just copies `libvulkan` when `VULKAN_SDK`
    is present, since the SDK dictates what files will be available and
    where. With any luck, this will finally fix the issue.
    
    Closes: #8

--- frameworks/vulkan-sys/build.rs
+++ frameworks/vulkan-sys/build.rs
@@ -1,5 +1,5 @@
 use std::env;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 fn main() {
   build::rerun_if_changed!("../../3rd-party/vulkan-headers");
@@ -12,8 +12,28 @@ fn main() {
 fn link_vulkan() {
   if let Some(search_path) = option_env!("VULKAN_SDK") {
     build::rustc_link_search!("{}/lib", search_path);
+
+    if cfg!(any(target_os = "macos", target_os = "ios")) {
+      let out = std::env::var("OUT_DIR").unwrap();
+      let _ = std::fs::create_dir(format!("{}/lib", out));
+      copy_file(search_path, &out, "libvulkan.dylib");
+      copy_file(search_path, &out, "libvulkan.1.dylib");
+    }
   }
   build::rustc_link_lib!("dylib=vulkan");
+}
+
+fn copy_file<P0, P1, P2>(search_path: P0, out: P1, file: P2)
+where
+  P0: AsRef<Path> + std::fmt::Display,
+  P1: AsRef<Path> + std::fmt::Display,
+  P2: AsRef<Path> + std::fmt::Display,
+{
+  std::fs::copy(
+    format!("{}/lib/{}", search_path, file),
+    format!("{}/lib/{}", out, file),
+  )
+  .unwrap_or_else(|_| panic!("Unable to copy {} to {}", file, out));
 }
 
 fn generate_vulkan_bindings() {
