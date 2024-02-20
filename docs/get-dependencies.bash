793f151465ca740a9c8301a9ae0c0460f7eb8c74
Matthew Rodusek <7519129+bitwizeshift@users.noreply.github.com>
2024-02-19 21:19:56 -0500

    ➕ Update mdbook-mermaid plugin version

--- docs/get-dependencies.bash
+++ docs/get-dependencies.bash
@@ -3,7 +3,7 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 OUT_DIR="${SCRIPT_DIR}"
 
-PERMALINK="https://raw.githubusercontent.com/badboy/mdbook-mermaid/5f87002c29d28fdfaad454024a80859e5d51c972/src/bin/assets/"
+PERMALINK="https://raw.githubusercontent.com/badboy/mdbook-mermaid/ca18754266fc1da8270bb5e7a9a9dcace3dfc944/src/bin/assets/"
 
 if [[ ! -f "${OUT_DIR}/mermaid.min.js" ]]; then
   wget "${PERMALINK}/mermaid.min.js" -O "${OUT_DIR}/mermaid.min.js"
