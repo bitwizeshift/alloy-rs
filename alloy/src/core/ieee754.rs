e6e91eb2ba78498e2aa9d9acd5d0ee99beed3f30
Matthew Rodusek <7519129+bitwizeshift@users.noreply.github.com>
2024-09-25 00:25:04 +0000

    Improve ieee 754 implementation
    The constant-evaluation floating-point operation module has been improved
    to include more tests, and introduce new functionality including
    big-endian support. This will enable various functions within Alloy
    to now be marked as constant expressions.

--- alloy/src/core/ieee754.rs
+++ alloy/src/core/ieee754.rs
@@ -1,53 +1,266 @@
 //! The ieee754 module provides utilities for working with IEEE 754
 //! floating-point numbers at compile-time.
 
-/// Module for working with 32-bit floating point numbers.
+/// Module for working with 32-bit floating point numbers at compile-time.
 pub mod f32 {
 
-  /// The mask for the sign bit of a 32-bit floating point number.
-  #[cfg(target_endian = "little")]
-  pub const SIGN_BIT_MASK_F32: u32 = 0x7FFFFFFF;
+  /// Determines the sign of a 32-bit floating point number.
+  #[inline(always)]
+  pub const fn sign(x: f32) -> bool {
+    let bits: u32 = unsafe { std::mem::transmute(x) };
+    sign_impl(bits)
+  }
 
-  /// The mask for the sign bit of a 32-bit floating point number.
-  #[cfg(target_endian = "big")]
-  pub const SIGN_BIT_MASK_F32: u32 = 0xFFFFFF7F;
+  /// The mask for the exponent bits of a 32-bit floating point number.
+  #[inline(always)]
+  pub const fn exponent(x: f32) -> u32 {
+    let bits: u32 = unsafe { std::mem::transmute(x) };
+    exponent_impl(bits)
+  }
 
-  /// Computes the absolute value of a 32-bit floating point number at compile-time.
+  /// The mask for the mantissa bits of a 32-bit floating point number.
+  #[inline(always)]
+  pub const fn mantissa(x: f32) -> u32 {
+    let bits: u32 = unsafe { std::mem::transmute(x) };
+    mantissa_impl(bits)
+  }
+
+  /// Determines if a 32-bit floating point number is a NaN.
+  #[inline(always)]
+  pub const fn is_nan(x: f32) -> bool {
+    let bits: u32 = unsafe { std::mem::transmute(x) };
+    is_nan_impl(bits)
+  }
+
+  /// Computes the parts of a 32-bit floating point number.
   ///
-  /// # Safety
-  ///
-  /// This function should largely be to safe to use, but requires that the user
-  /// ensure that numbers are not NaN or subnormal numbers. This ultimately should
-  /// only be used in cases where constant-time operations are desired (e.g. during
-  /// construction).
+  /// This returns the sign of the float, the exponent, and the mantissa.
   ///
   /// # Arguments
   ///
   /// * `x` - The floating point number.
-  pub const unsafe fn abs(x: f32) -> f32 {
-    let bits: u32 = std::mem::transmute(x);
-    // NOTE: This ignores NaN and subnormal numbers, which is largely fine for
-    // the cases of where this is intended to be used.
-    std::mem::transmute(bits & SIGN_BIT_MASK_F32)
+  #[inline(always)]
+  pub const fn parts(x: f32) -> (bool, u32, u32) {
+    let bits: u32 = unsafe { std::mem::transmute(x) };
+    (sign_impl(bits), exponent_impl(bits), mantissa_impl(bits))
+  }
+
+  /// Returns the merged exponent and mantissa of a 32-bit floating point number.
+  #[inline(always)]
+  pub const fn exponent_mantissa(x: f32) -> u32 {
+    let bits: u32 = unsafe { std::mem::transmute(x) };
+    exponent_mantissa_impl(bits)
+  }
+
+  /// Computes the absolute value of a 32-bit floating point number at compile-time.
+  ///
+  /// NOTE: Unlike the standard [`f32::abs`] function, this function will
+  /// return the absolute value of a floating point number, even if it is a NaN.
+  /// This means that the representation of the NaN may change by calling this,
+  /// even though the value is still considered a NaN.
+  ///
+  /// # Arguments
+  ///
+  /// * `x` - The floating point number.
+  pub const fn abs(x: f32) -> f32 {
+    // SAFETY: Transmuting float to u32 is safe, it's just generally inadvisable.
+    let bits: u32 = unsafe { std::mem::transmute(x) };
+
+    // SAFETY: This is safe because the bits are transmuted back to a float.
+    unsafe { std::mem::transmute(abs_impl(bits)) }
   }
 
   /// Computes the negation of a 32-bit floating point number at compile-time.
   ///
-  /// # Safety
-  ///
-  /// This function should largely be to safe to use, but requires that the user
-  /// ensure that numbers are not NaN or subnormal numbers. This ultimately should
-  /// only be used in cases where constant-time operations are desired (e.g. during
-  /// construction).
+  /// NOTE: Unlike the standard [`Neg`] trait for [`f32`], this function will
+  /// return the negation of a floating point number, even if it is a NaN. This
+  /// means that the representation of the NaN may change by calling this, even
+  /// though the value is still a NaN.
   ///
   /// # Arguments
   ///
   /// * `x` - The floating point number.
-  pub const unsafe fn neg(x: f32) -> f32 {
-    let bits: u32 = std::mem::transmute(x);
-    // NOTE: This ignores NaN and subnormal numbers, which is largely fine for
-    // the cases of where this is intended to be used.
-    std::mem::transmute(bits ^ !SIGN_BIT_MASK_F32)
+  ///
+  /// [`Neg`]: std::ops::Neg
+  pub const fn neg(x: f32) -> f32 {
+    // SAFETY: Transmuting float to u32 is safe, it's just generally inadvisable.
+    let bits: u32 = unsafe { std::mem::transmute(x) };
+
+    // SAFETY: This is safe because the bits are transmuted back to a float.
+    unsafe { std::mem::transmute(neg_impl(bits)) }
+  }
+
+  /// Determines if a 32-bit floating point number is less than another at
+  /// compile-time.
+  ///
+  /// This function is _not_ recommended in general code, since its performance
+  /// characteristics will not be equivalent to the standard library's
+  /// [`PartialOrd`] implementation. However it is useful for `const` contexts
+  /// so that it can be used in [`assert`]-like situations.
+  ///
+  /// # Arguments
+  ///
+  /// * `lhs` - The left-hand side of the comparison.
+  /// * `rhs` - The right-hand side of the comparison.
+  pub const fn lt(lhs: f32, rhs: f32) -> bool {
+    // SAFETY: Transmuting float to u32 is "safe", it's just generally inadvisable.
+    // They both have the same size and alignment, and this is how to_bits is
+    // implemented in the standard library -- however to_bits does handle a few
+    // extra cases on tier-2 platforms that might change the f32 representation
+    // at runtime. These don't apply to Alloy, so this is safe for our uses.
+    //
+    // This will be updated to use to_bits when able to.
+    let lhs_bits: u32 = unsafe { std::mem::transmute(lhs) };
+    let rhs_bits: u32 = unsafe { std::mem::transmute(rhs) };
+
+    if is_nan(lhs) || is_nan(rhs) {
+      return false;
+    }
+    if sign_impl(lhs_bits) && !sign_impl(rhs_bits) {
+      return true;
+    }
+    if exponent_impl(lhs_bits) < exponent_impl(rhs_bits) {
+      return true;
+    }
+    if mantissa_impl(lhs_bits) < mantissa_impl(rhs_bits) {
+      return true;
+    }
+    false
+  }
+
+  /// Determines if a 32-bit floating point number is equal to another at
+  /// compile-time.
+  ///
+  /// This function is _not_ recommended in general code, since its performance
+  /// characteristics will not be equivalent to the standard library's
+  /// [`PartialEq`] implementation. However it is useful for `const` contexts
+  /// so that it can be used in [`assert`]-like situations.
+  ///
+  /// # Arguments
+  ///
+  /// * `lhs` - The left-hand side of the comparison.
+  /// * `rhs` - The right-hand side of the comparison.
+  pub const fn eq(lhs: f32, rhs: f32) -> bool {
+    // SAFETY: Transmuting float to u32 is safe, it's just generally inadvisable.
+    // They both have the same size and alignment, and this is how to_bits is
+    // implemented in the standard library -- it's just not const.
+    let lhs_bits: u32 = unsafe { std::mem::transmute(lhs) };
+    let rhs_bits: u32 = unsafe { std::mem::transmute(rhs) };
+
+    if is_nan(lhs) || is_nan(rhs) {
+      return false;
+    }
+
+    let lhs_content = exponent_mantissa_impl(lhs_bits);
+    let rhs_content = exponent_mantissa_impl(rhs_bits);
+    if sign_impl(lhs_bits) != sign_impl(rhs_bits) {
+      return lhs_content == 0 && rhs_content == 0;
+    }
+    lhs_content == rhs_content
+  }
+
+  // Private impl
+
+  #[inline(always)]
+  const fn abs_le(bits: u32) -> u32 {
+    bits & !0x80000000
+  }
+
+  #[cfg(target_endian = "little")]
+  #[inline(always)]
+  const fn abs_impl(bits: u32) -> u32 {
+    abs_le(bits)
+  }
+
+  #[cfg(target_endian = "big")]
+  #[inline(always)]
+  const fn abs_impl(bits: u32) -> u32 {
+    abs_le(bits.swap_bytes())
+  }
+
+  #[inline(always)]
+  const fn neg_le(bits: u32) -> u32 {
+    bits ^ 0x80000000
+  }
+
+  #[cfg(target_endian = "little")]
+  #[inline(always)]
+  const fn neg_impl(bits: u32) -> u32 {
+    neg_le(bits)
+  }
+
+  #[cfg(target_endian = "big")]
+  #[inline(always)]
+  const fn neg_impl(bits: u32) -> u32 {
+    neg_le(bits.swap_bytes())
+  }
+
+  #[cfg(target_endian = "little")]
+  #[inline(always)]
+  const fn sign_impl(bits: u32) -> bool {
+    bits & 0x80000000 != 0
+  }
+
+  #[cfg(target_endian = "big")]
+  #[inline(always)]
+  const fn sign_impl(bits: u32) -> bool {
+    bits & 0x00000080 != 0
+  }
+
+  #[inline(always)]
+  const fn exponent_le(bits: u32) -> u32 {
+    (bits & 0x7F800000) >> 23
+  }
+
+  #[cfg(target_endian = "little")]
+  #[inline(always)]
+  const fn exponent_impl(bits: u32) -> u32 {
+    exponent_le(bits)
+  }
+
+  #[cfg(target_endian = "big")]
+  #[inline(always)]
+  const fn exponent_impl(bits: u32) -> u32 {
+    exponent_le(bits.swap_bytes())
+  }
+
+  #[inline(always)]
+  const fn mantissa_le(bits: u32) -> u32 {
+    bits & 0x007FFFFF
+  }
+
+  #[cfg(target_endian = "little")]
+  #[inline(always)]
+  const fn mantissa_impl(bits: u32) -> u32 {
+    mantissa_le(bits)
+  }
+
+  #[cfg(target_endian = "big")]
+  #[inline(always)]
+  const fn mantissa_impl(bits: u32) -> u32 {
+    mantissa_le(bits.swap_bytes())
+  }
+
+  #[inline(always)]
+  const fn is_nan_impl(bits: u32) -> bool {
+    exponent_impl(bits) == 0xFF && mantissa_impl(bits) != 0
+  }
+
+  #[inline(always)]
+  const fn exponent_mantissa_le(bits: u32) -> u32 {
+    bits & 0x7FFFFFFF
+  }
+
+  #[cfg(target_endian = "little")]
+  #[inline(always)]
+  const fn exponent_mantissa_impl(bits: u32) -> u32 {
+    exponent_mantissa_le(bits)
+  }
+
+  #[cfg(target_endian = "big")]
+  #[inline(always)]
+  const fn exponent_mantissa_impl(bits: u32) -> u32 {
+    exponent_mantissa_le(bits.swap_bytes())
   }
 }
 
--- alloy/src/core/ieee754.test.rs
+++ alloy/src/core/ieee754.test.rs
@@ -1,48 +1,32 @@
+use std::ops::Neg;
+
 use super::*;
 
 #[test]
 fn f32_abs() {
   struct TestCase {
     input: f32,
-    expected: f32,
   }
 
   let test_cases = [
-    TestCase {
-      input: 1.0,
-      expected: 1.0,
-    },
-    TestCase {
-      input: -1.0,
-      expected: 1.0,
-    },
-    TestCase {
-      input: 0.0,
-      expected: 0.0,
-    },
-    TestCase {
-      input: -0.0,
-      expected: 0.0,
-    },
+    TestCase { input: 1.0 },
+    TestCase { input: -1.0 },
+    TestCase { input: 0.0 },
+    TestCase { input: -0.0 },
     TestCase {
       input: f32::INFINITY,
-      expected: f32::INFINITY,
     },
     TestCase {
       input: f32::NEG_INFINITY,
-      expected: f32::INFINITY,
     },
-    TestCase {
-      input: -42.371,
-      expected: 42.371,
-    },
+    TestCase { input: -42.371 },
   ];
 
   for test in test_cases {
-    // SAFETY: The test cases are all valid floating point numbers.
-    let result = unsafe { f32::abs(test.input) };
+    let expected = test.input.abs();
+    let result = f32::abs(test.input);
 
-    assert_eq!(result, test.expected);
+    assert_eq!(result, expected);
   }
 }
 
@@ -50,44 +34,129 @@ fn f32_abs() {
 fn f32_neg() {
   struct TestCase {
     input: f32,
-    expected: f32,
   }
 
   let test_cases = [
-    TestCase {
-      input: 1.0,
-      expected: -1.0,
-    },
-    TestCase {
-      input: -1.0,
-      expected: 1.0,
-    },
-    TestCase {
-      input: 0.0,
-      expected: -0.0,
-    },
-    TestCase {
-      input: -0.0,
-      expected: 0.0,
-    },
+    TestCase { input: 1.0 },
+    TestCase { input: -1.0 },
+    TestCase { input: 0.0 },
+    TestCase { input: -0.0 },
     TestCase {
       input: f32::INFINITY,
-      expected: f32::NEG_INFINITY,
     },
     TestCase {
       input: f32::NEG_INFINITY,
-      expected: f32::INFINITY,
+    },
+    TestCase { input: -42.371 },
+  ];
+
+  for test in test_cases {
+    let expected = test.input.neg();
+    let result = f32::neg(test.input);
+
+    assert_eq!(result, expected);
+  }
+}
+
+#[test]
+fn eq() {
+  struct TestCase {
+    lhs: f32,
+    rhs: f32,
+  }
+
+  let test_cases = [
+    TestCase { lhs: 1.0, rhs: 1.0 },
+    TestCase {
+      lhs: 1.0,
+      rhs: -1.0,
+    },
+    TestCase { lhs: 0.0, rhs: 0.0 },
+    TestCase {
+      lhs: 0.0,
+      rhs: -0.0,
     },
     TestCase {
-      input: -42.371,
-      expected: 42.371,
+      lhs: f32::INFINITY,
+      rhs: f32::INFINITY,
+    },
+    TestCase {
+      lhs: f32::INFINITY,
+      rhs: f32::NEG_INFINITY,
+    },
+    TestCase {
+      lhs: f32::NAN,
+      rhs: f32::NAN,
+    },
+    TestCase {
+      lhs: 42.371,
+      rhs: 42.371,
     },
   ];
 
   for test in test_cases {
-    // SAFETY: The test cases are all valid floating point numbers.
-    let result = unsafe { f32::neg(test.input) };
+    let expected = test.lhs == test.rhs;
+    let result = f32::eq(test.lhs, test.rhs);
 
-    assert_eq!(result, test.expected);
+    assert_eq!(result, expected, "{} == {}", test.lhs, test.rhs);
+  }
+}
+
+#[test]
+fn lt() {
+  struct TestCase {
+    lhs: f32,
+    rhs: f32,
+  }
+
+  let test_cases = [
+    TestCase { lhs: 1.0, rhs: 1.0 },
+    TestCase {
+      lhs: -1.0,
+      rhs: 1.0,
+    },
+    TestCase {
+      lhs: 1.0,
+      rhs: -1.0,
+    },
+    TestCase {
+      lhs: 0.0,
+      rhs: -0.0,
+    },
+    TestCase {
+      lhs: f32::INFINITY,
+      rhs: f32::INFINITY,
+    },
+    TestCase {
+      lhs: f32::INFINITY,
+      rhs: f32::NEG_INFINITY,
+    },
+    TestCase {
+      lhs: f32::NEG_INFINITY,
+      rhs: f32::INFINITY,
+    },
+    TestCase {
+      lhs: f32::NAN,
+      rhs: f32::NAN,
+    },
+    TestCase {
+      lhs: f32::NAN,
+      rhs: 42.360,
+    },
+    TestCase {
+      lhs: 42.360,
+      rhs: f32::NAN,
+    },
+    TestCase {
+      lhs: 42.371,
+      rhs: 42.371,
+    },
+  ];
+
+  for test in test_cases {
+    let expected = test.lhs < test.rhs;
+    let result = f32::lt(test.lhs, test.rhs);
+
+    assert_eq!(result, expected, "{} < {}", test.lhs, test.rhs);
   }
 }
