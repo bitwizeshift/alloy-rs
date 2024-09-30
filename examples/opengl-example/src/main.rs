824d77f569329571256fa74e28d904b6d8a84628
Matthew Rodusek <7519129+bitwizeshift@users.noreply.github.com>
2024-09-30 18:19:02 +0000

    Add Rust wrapper around imgui framework

--- examples/opengl-example/src/main.rs
+++ examples/opengl-example/src/main.rs
@@ -1,3 +1,4 @@
+use alloy::math::mat::Matrix4;
 use astd::ffi::cstr;
 
 extern "C" fn error_callback(error: i32, description: *const i8) {
@@ -6,9 +7,43 @@ extern "C" fn error_callback(error: i32, description: *const i8) {
   });
 }
 
+const VERTEX_SHADER: &str = include_str!("../assets/shaders/triangle/shader.vert");
+const FRAGMENT_SHADER: &str = include_str!("../assets/shaders/triangle/shader.frag");
+
+fn log_opengl_preamble(logger: &mut log::Logger) {
+  let (major, minor) = gl::info::supported_version();
+  if let Some(err) = gl::error::Error::last_error() {
+    log::error!(logger, "OpenGL error: {}", err);
+  }
+  log::debug!(logger, "OpenGL supported version: {}.{}", major, minor);
+  log::debug!(logger, "OpenGL version: {}", gl::info::version());
+  log::debug!(logger, "OpenGL renderer: {}", gl::info::renderer());
+  log::debug!(logger, "OpenGL vendor: {}", gl::info::vendor());
+  log::debug!(
+    logger,
+    "OpenGL shading language version: {}",
+    gl::info::shading_language_version()
+  );
+  log::debug!(logger, "OpenGL extensions:");
+  for ext in gl::info::extensions() {
+    log::debug!(logger, "- {}", ext);
+  }
+}
+
+fn log_openal_preamble(logger: &mut log::Logger) {
+  if openal::has_extension(cstr!("ALC_ENUMERATION_EXT")) {
+    log::debug!(logger, "OpenAL has enumeration extension!")
+  } else {
+    log::debug!(logger, "OpenAL does NOT have enumeration extension!")
+  }
+}
+
 fn main() {
   let mut logger: log::Logger = log::Logger::new("log");
   logger.sink(std::io::stderr());
+
+  log::debug!(logger, "vertex: {}", VERTEX_SHADER);
+  log::debug!(logger, "fragment: {}", FRAGMENT_SHADER);
 
   let glfw = glfw::Instance::new().expect("GLFW should be initialized");
   unsafe {
@@ -32,77 +67,144 @@ fn main() {
       std::ptr::null_mut(),
     )
   };
+  // tar::read_tar(&mut std::fs::File::open("out.tar").unwrap()).unwrap();
   let window = glfw::Window::from_c(raw_window);
   window.show();
   window.make_context_current();
 
-  let _glew = glew::Instance::new().expect("GLEW should be initialized");
-  let (major, minor) = gl::info::supported_version();
-  if let Some(err) = gl::error::Error::last_error() {
-    log::error!(logger, "OpenGL error: {}", err);
-  }
-  log::debug!(logger, "OpenGL supported version: {}.{}", major, minor);
-  log::debug!(logger, "OpenGL version: {}", gl::info::version());
-  log::debug!(logger, "OpenGL renderer: {}", gl::info::renderer());
-  log::debug!(logger, "OpenGL vendor: {}", gl::info::vendor());
-  log::debug!(
-    logger,
-    "OpenGL shading language version: {}",
-    gl::info::shading_language_version()
-  );
-  log::debug!(logger, "OpenGL extensions:");
-  for ext in gl::info::extensions() {
-    log::debug!(logger, "- {}", ext);
-  }
+  let _model =
+    alloy::model::obj::Model::from_file("examples/opengl-example/assets/teapot.obj").unwrap();
 
+  let _glew = glew::Instance::new().expect("GLEW should be initialized");
   let _device = openal::Device::open_default();
-  if openal::has_extension(cstr!("ALC_ENUMERATION_EXT")) {
-    log::debug!(logger, "OpenAL has enumeration extension!")
-  } else {
-    log::debug!(logger, "OpenAL does NOT have enumeration extension!")
-  }
-  let context = imgui::Context::new();
+
+  log_opengl_preamble(&mut logger);
+  log_openal_preamble(&mut logger);
+  let _context = imgui::Context::new();
+  imgui::io::set_ini_filename_cstr(cstr!("alloy.ini"));
+  imgui::io::set_io_config(
+    imgui::io::ConfigFlags::default()
+      .allow_gamepad_navigation(true)
+      .allow_keyboard_navigation(true)
+      .allow_mouse_navigation(true)
+      .allow_docking(true),
+  );
   unsafe {
-    imgui_sys::backend::opengl::imgui_opengl3_init(cstr!("#version 150").as_ptr());
+    imgui_sys::backend::opengl::imgui_opengl3_init(cstr!("#version 410").as_ptr());
     imgui_sys::backend::glfw::imgui_glfw_init_for_opengl(window.ptr_mut(), true);
   }
 
-  // let vertices: [f32; 9] = [-0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.0, 0.5, 0.0];
-  // let indices: [u32; 3] = [0, 1, 2];
+  const VERTICES: [f32; 9] = [
+    -1.0, -1.0, 0.0, //
+    1.0, -1.0, 0.0, //
+    0.0, 1.0, 0.0, //
+  ];
+  const INDICES: [u32; 3] = [
+    0, 1, 2, // first Triangle
+  ];
 
-  // unsafe {
-  //   // let vao = glew::gl::vertex::generate_vertex_array();
-  //   // glew::gl::vertex::bind_vertex_array(vao);
+  let mut m_vao: gl::c::GLuint = 0;
+  let mut m_vbo: gl::c::GLuint = 0;
+  let mut m_ebo: gl::c::GLuint = 0;
 
-  //   // let vbo = glew::gl::vertex::generate_buffer();
-  //   // glew::gl::vertex::bind_buffer(glew::c::GL_ARRAY_BUFFER, vbo);
-  //   // glew::gl::vertex::buffer_data(
-  //   //   glew::c::GL_ARRAY_BUFFER,
-  //   //   (vertices.len() * std::mem::size_of::<f32>()) as _,
-  //   //   vertices.as_ptr() as _,
-  //   //   glew::c::GL_STATIC_DRAW,
-  //   // )
-  //   // glew::c::glBufferData(
-  //   //   glew::c::GL_ARRAY_BUFFER,
-  //   //   (vertices.len() * std::mem::size_of::<f32>()) as glew::c::GLsizeiptr,
-  //   //   vertices.as_ptr() as *const glew::c::GLvoid,
-  //   //   glew::c::GL_STATIC_DRAW,
-  //   // );
-  // }
+  let mut vertex_shader = gl::shader::Shader::new(gl::shader::ShaderKind::Vertex);
+  vertex_shader.shader_source(VERTEX_SHADER);
+  vertex_shader.compile().unwrap();
 
+  let mut fragment_shader = gl::shader::Shader::new(gl::shader::ShaderKind::Fragment);
+  fragment_shader.shader_source(FRAGMENT_SHADER);
+  fragment_shader.compile().unwrap();
+
+  let mut shader_program = gl::shader::Program::new();
+  shader_program.attach_shader(&vertex_shader);
+  shader_program.attach_shader(&fragment_shader);
+  shader_program.link().unwrap();
+
+  unsafe {
+    gl::c::glGenVertexArrays(1, &mut m_vao as _);
+
+    let mut buffers: [gl::c::GLuint; 2] = [0, 0];
+
+    gl::c::glGenBuffers(2, buffers.as_mut_ptr());
+    m_vbo = buffers[0];
+    m_ebo = buffers[1];
+    (_, _) = (m_vbo, m_ebo);
+
+    gl::c::glBindVertexArray(m_vao);
+    gl::c::glBindBuffer(gl::c::GL_ARRAY_BUFFER, m_vbo);
+    gl::c::glBufferData(
+      gl::c::GL_ARRAY_BUFFER,
+      (VERTICES.len() * 4) as _,
+      VERTICES.as_ptr() as _,
+      gl::c::GL_STATIC_DRAW,
+    );
+    gl::c::glVertexAttribPointer(0, 3, glew::c::GL_FLOAT, false, 0, std::ptr::null());
+    gl::c::glEnableVertexAttribArray(0);
+
+    gl::c::glBindBuffer(gl::c::GL_ELEMENT_ARRAY_BUFFER, m_ebo);
+    gl::c::glBufferData(
+      gl::c::GL_ELEMENT_ARRAY_BUFFER,
+      (INDICES.len() * 4) as _,
+      INDICES.as_ptr() as _,
+      gl::c::GL_STATIC_DRAW,
+    );
+  }
+
+  log::debug!(logger, "m_vao: {}", m_vao);
+  log::debug!(logger, "m_vbo: {}", m_vbo);
+  log::debug!(logger, "m_ebo: {}", m_ebo);
+
+  use alloy::model::clip;
+  let projection = alloy::model::projection::Projection::orthographic(clip::ClipSpace {
+    depth: clip::Depth::new(-2.0, 2.0),
+    horizontal: clip::Horizontal::uniform(2.0),
+    vertical: clip::Vertical::uniform(2.0),
+  });
+
+  let projection_matrix = projection.to_matrix4();
+  let view_matrix = Matrix4::identity();
+
+  let mut transform = alloy::model::transform::Transform::default();
   while !window.should_close() {
+    gl::clear(gl::ClearBits::none().color().depth());
+    gl::clear_color(0.0, 0.0, 0.0, 1.0);
+
+    shader_program.bind();
+    let transform_loc = shader_program.uniform_location("transform");
+    let projection_loc = shader_program.uniform_location("projection");
+    let view_loc = shader_program.uniform_location("view");
+    unsafe {
+      shader_program.set_uniform_matrix4_unchecked(
+        transform_loc,
+        1,
+        transform.transform().as_ptr(),
+      );
+      shader_program.set_uniform_matrix4_unchecked(projection_loc, 1, projection_matrix.as_ptr());
+      shader_program.set_uniform_matrix4_unchecked(view_loc, 1, view_matrix.as_ptr());
+    }
+    transform.rotate_pitch(alloy::math::Degree::new(1.0));
+
+    unsafe {
+      gl::c::glBindBuffer(gl::c::GL_ARRAY_BUFFER, m_vbo);
+      gl::c::glDrawArrays(glew::c::GL_TRIANGLES, 0, (VERTICES.len() / 3) as _);
+    }
+
+    // let fps = 1.0 / delta.as_secs_f32();
     unsafe {
       imgui_sys::backend::opengl::imgui_opengl3_new_frame();
       imgui_sys::backend::glfw::imgui_glfw_new_frame();
-      imgui_sys::igNewFrame();
+      imgui::new_frame();
+
       imgui_sys::igShowDemoWindow(&mut true as *mut _);
       imgui_sys::igBegin(cstr!("Hello, world!").as_ptr(), std::ptr::null_mut(), 0);
-      imgui_sys::igText(cstr!("This is some useful text.").as_ptr());
+      imgui::text!("This is some useful text.");
+      // imgui::text!("FPS {:6.2}", fps);
       imgui_sys::igEnd();
       imgui_sys::igRender();
-      gl::clear(gl::ClearBits::COLOR);
+      imgui_sys::igUpdatePlatformWindows();
       imgui_sys::backend::opengl::imgui_opengl3_render_draw_data(imgui_sys::igGetDrawData());
     }
+
     window.swap_buffers();
     glfw.poll_events();
   }
--- frameworks/imgui/src/lib.rs
+++ frameworks/imgui/src/lib.rs
@@ -21,6 +21,323 @@ pub mod c {
   pub use imgui_sys::*;
 }
 
+/// This module provides a wrapper around the [`c::ImGuiWindowFlags`] type.
+#[derive(Default, Copy, Clone, Debug)]
+pub struct WindowFlags {
+  flags: crate::c::ImGuiWindowFlags,
+  show_window_close: bool,
+}
+
+impl WindowFlags {
+  /// Returns the raw window flags.
+  pub fn flags(&self) -> crate::c::ImGuiWindowFlags {
+    self.flags
+  }
+
+  /// Sets the window to be invisible.
+  pub fn no_title_bar(self, enable: bool) -> Self {
+    self.set_flag(
+      crate::c::ImGuiWindowFlags__ImGuiWindowFlags_NoTitleBar,
+      enable,
+    )
+  }
+
+  /// Sets the window to be invisible.
+  pub fn no_scrollbar(self, enable: bool) -> Self {
+    self.set_flag(
+      crate::c::ImGuiWindowFlags__ImGuiWindowFlags_NoScrollbar,
+      enable,
+    )
+  }
+
+  /// Sets the window to be invisible.
+  pub fn no_move(self, enable: bool) -> Self {
+    self.set_flag(crate::c::ImGuiWindowFlags__ImGuiWindowFlags_NoMove, enable)
+  }
+
+  /// Sets the window to be invisible.
+  pub fn no_resize(self, enable: bool) -> Self {
+    self.set_flag(
+      crate::c::ImGuiWindowFlags__ImGuiWindowFlags_NoResize,
+      enable,
+    )
+  }
+
+  /// Sets the window to be invisible.
+  pub fn no_collapse(self, enable: bool) -> Self {
+    self.set_flag(
+      crate::c::ImGuiWindowFlags__ImGuiWindowFlags_NoCollapse,
+      enable,
+    )
+  }
+
+  /// Sets the window to be invisible.
+  pub fn no_nav(self, enable: bool) -> Self {
+    self.set_flag(crate::c::ImGuiWindowFlags__ImGuiWindowFlags_NoNav, enable)
+  }
+
+  /// Sets the window to be invisible.
+  pub fn no_background(self, enable: bool) -> Self {
+    self.set_flag(
+      crate::c::ImGuiWindowFlags__ImGuiWindowFlags_NoBackground,
+      enable,
+    )
+  }
+
+  /// Sets the window to be invisible.
+  pub fn no_bring_to_front_on_focus(self, enable: bool) -> Self {
+    self.set_flag(
+      crate::c::ImGuiWindowFlags__ImGuiWindowFlags_NoBringToFrontOnFocus,
+      enable,
+    )
+  }
+
+  /// Whether to show the window close button (default: false)
+  pub fn show_window_close(self, enable: bool) -> Self {
+    Self {
+      flags: self.flags,
+      show_window_close: enable,
+    }
+  }
+
+  const fn set_flag(self, flag: u32, enable: bool) -> Self {
+    if enable {
+      Self {
+        flags: self.flags | flag as i32,
+        show_window_close: self.show_window_close,
+      }
+    } else {
+      Self {
+        flags: self.flags & !flag as i32,
+        show_window_close: self.show_window_close,
+      }
+    }
+  }
+}
+
+///
+pub trait Renderable {
+  ///
+  fn render(&self);
+}
+
+/// Creates a window with the given name and flags.
+pub fn begin(name: &str, open: Option<&mut bool>, flags: WindowFlags) -> bool {
+  unsafe {
+    let name = std::ffi::CString::new(name).unwrap();
+    let show_window_close = match open {
+      Some(open) => open as *mut _,
+      None => std::ptr::null_mut(),
+    };
+    c::igBegin(name.as_ptr(), show_window_close, flags.flags())
+  }
+}
+
+///
+#[inline(always)]
+pub fn begin_default(name: &str) -> bool {
+  begin(name, None, WindowFlags::default())
+}
+
+/// This module provides I/O functionality that wraps the [`c::ImGuiIO`] object.
+///
+/// [`c::ImGuiIO`]: crate::c::ImGuiIO
+pub mod io {
+  use std::ffi::CStr;
+
+  /// The configuration flags for the ImGui I/O context.
+  #[derive(Copy, Clone, Debug, Default, PartialEq, Eq, PartialOrd, Ord)]
+  pub struct ConfigFlags(crate::c::ImGuiConfigFlags);
+
+  impl ConfigFlags {
+    /// Returns the configuration flags.
+    pub fn flags(&self) -> crate::c::ImGuiConfigFlags {
+      self.0
+    }
+
+    /// Enables or disables the keyboard navigation
+    pub fn allow_keyboard_navigation(self, enable: bool) -> Self {
+      self.set_flag(
+        crate::c::ImGuiConfigFlags__ImGuiConfigFlags_NavEnableKeyboard,
+        enable,
+      )
+    }
+
+    /// Enables or disables the gamepad navigation
+    pub fn allow_gamepad_navigation(self, enable: bool) -> Self {
+      self.set_flag(
+        crate::c::ImGuiConfigFlags__ImGuiConfigFlags_NavEnableGamepad,
+        enable,
+      )
+    }
+
+    /// Enables or disables the mouse navigation
+    pub fn allow_mouse_navigation(self, enable: bool) -> Self {
+      self.set_flag(
+        crate::c::ImGuiConfigFlags__ImGuiConfigFlags_NavEnableSetMousePos,
+        enable,
+      )
+    }
+
+    /// Enables or disables docking support.
+    pub fn allow_docking(self, enable: bool) -> Self {
+      self.set_flag(
+        crate::c::ImGuiConfigFlags__ImGuiConfigFlags_DockingEnable,
+        enable,
+      )
+    }
+
+    /// Enables or disables viewports.
+    pub fn allow_viewports(self, enable: bool) -> Self {
+      self.set_flag(
+        crate::c::ImGuiConfigFlags__ImGuiConfigFlags_ViewportsEnable,
+        enable,
+      )
+    }
+
+    /// Sets whether the mouse should be disabled.
+    pub fn no_mouse(self, enable: bool) -> Self {
+      self.set_flag(crate::c::ImGuiConfigFlags__ImGuiConfigFlags_NoMouse, enable)
+    }
+
+    /// Sets whether the mouse cursor should change.
+    pub fn no_mouse_cursor_change(self, enable: bool) -> Self {
+      self.set_flag(
+        crate::c::ImGuiConfigFlags__ImGuiConfigFlags_NoMouseCursorChange,
+        enable,
+      )
+    }
+
+    /// Sets whether viewports should be DPI-scaled.
+    pub fn dpi_scale_viewports(self, enable: bool) -> Self {
+      self.set_flag(
+        crate::c::ImGuiConfigFlags__ImGuiConfigFlags_DpiEnableScaleViewports,
+        enable,
+      )
+    }
+
+    /// Sets whether fonts should be DPI-scaled.
+    pub fn dpi_scale_fonts(self, enable: bool) -> Self {
+      self.set_flag(
+        crate::c::ImGuiConfigFlags__ImGuiConfigFlags_DpiEnableScaleFonts,
+        enable,
+      )
+    }
+
+    /// Sets whether the color profile is SRGB
+    pub fn set_srgb(self, enable: bool) -> Self {
+      self.set_flag(crate::c::ImGuiConfigFlags__ImGuiConfigFlags_IsSRGB, enable)
+    }
+
+    /// Sets whether the application is a touch screen.
+    pub fn set_touchscreen(self, enable: bool) -> Self {
+      self.set_flag(
+        crate::c::ImGuiConfigFlags__ImGuiConfigFlags_IsTouchScreen,
+        enable,
+      )
+    }
+    const fn set_flag(self, flag: u32, enable: bool) -> Self {
+      if enable {
+        Self(self.0 | flag as i32)
+      } else {
+        Self(self.0 & !flag as i32)
+      }
+    }
+  }
+
+  /// Sets the configuration flags for the ImGui I/O context.
+  pub fn set_io_config(config: ConfigFlags) {
+    unsafe {
+      let io = crate::c::igGetIO();
+      (*io).ConfigFlags = config.flags();
+    }
+  }
+
+  /// Sets the INI filename that ImGui will use.
+  ///
+  /// # Parameters
+  ///
+  /// * `filename` - The filename to set.
+  pub fn set_ini_filename_cstr(filename: &'static CStr) {
+    use crate::c;
+
+    // SAFETY: This is safe because the pointer is valid for the lifetime of the
+    // CStr, and because io is always a valid pointer.
+    unsafe {
+      let io = c::igGetIO();
+      (*io).IniFilename = filename.as_ptr();
+    }
+  }
+
+  /// Sets the INI filename that ImGui will use.
+  ///
+  /// # Safety
+  ///
+  /// The input string must be a valid static C string.
+  ///
+  /// # Parameters
+  ///
+  /// * `filename` - The filename to set.
+  pub unsafe fn set_ini_filename(filename: &'static str) {
+    let cstr = std::ffi::CStr::from_ptr(filename.as_ptr() as *const _);
+    set_ini_filename_cstr(cstr);
+  }
+}
+
+/// This module provides a wrapper around the underlying imgui implementation.
+pub mod backend {
+  use crate::c;
+  use astd::ffi::cstr;
+
+  /// A backend is a platform-specific implementation of the imgui renderer.
+  pub trait Backend {
+    /// Initializes the backend.
+    fn init_for_window(&self, window: &mut glfw::Window);
+
+    /// Creates a new frame.
+    fn new_frame(&self);
+
+    /// Renders the frame.
+    fn render(&self);
+
+    /// Shuts down the backend.
+    fn shutdown(&self);
+  }
+
+  /// The OpenGL backend.
+  #[cfg(feature = "opengl")]
+  pub struct OpenGL;
+
+  #[cfg(feature = "opengl")]
+  impl Backend for OpenGL {
+    fn init_for_window(&self, window: &mut glfw::Window) {
+      unsafe {
+        _ = c::backend::glfw::imgui_glfw_init_for_opengl(window.ptr_mut(), true);
+        _ = c::backend::opengl::imgui_opengl3_init(cstr!("#version 410").as_ptr());
+      }
+    }
+
+    fn new_frame(&self) {
+      unsafe {
+        _ = c::backend::opengl::imgui_opengl3_new_frame();
+        _ = c::backend::glfw::imgui_glfw_new_frame();
+      }
+    }
+
+    fn render(&self) {
+      unsafe {
+        _ = c::backend::opengl::imgui_opengl3_render_draw_data(c::igGetDrawData());
+      }
+    }
+
+    fn shutdown(&self) {
+      unsafe {
+        _ = c::backend::opengl::imgui_opengl3_shutdown();
+      }
+    }
+  }
+}
+
 /// The context is the default setup for Imgui.
 pub struct Context(*mut c::ImGuiContext);
 
@@ -41,4 +358,31 @@ impl Drop for Context {
   fn drop(&mut self) {
     unsafe { c::igDestroyContext(self.0) }
   }
+}
+
+/// Begins a new frame.
+pub fn new_frame() {
+  unsafe {
+    c::igNewFrame();
+  }
+}
+
+/// Ends the current frame.
+pub fn end_frame() {
+  unsafe {
+    c::igEndFrame();
+  }
+}
+
+///
+#[macro_export]
+macro_rules! text {
+  ($($arg:tt)*) => {
+    unsafe {
+      use std::ffi::CString;
+      let s = format!($($arg)*);
+      let cs = CString::new(s).unwrap();
+      $crate::c::igText(cs.as_ptr());
+    }
+  };
 }
